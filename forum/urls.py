b09d7adf0da154add391c6f8a65ffa022a1247e7
Leonardo Bianco <leonardo.bianco@outlook.com>
2019-01-29 21:17:45 +0100

    Feature: aggiunto lo scheletro della visualizzazione di una singola discussione. Aggiunto css date post, fatta la vista per la nuova pagina.

--- forum/urls.py
+++ forum/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns =[
     path("nuova-sezione/", views.CreaSezione.as_view(), name="crea_sezione"),
     path("sezione/<int:pk>", views.visualizzaSezione, name="sezione_view"),
-    path("sezione/<int:pk>/crea-discussione", views.creaDiscussione, name="crea_discussione")
+    path("sezione/<int:pk>/crea-discussione", views.creaDiscussione, name="crea_discussione"),
+    path("discussione/<int:pk>", views.visualizzaDiscussione, name="discussione_view")
+
 
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView
 # Create your views here.
-from .models import Post, Sezione
+from .models import Discussione, Post, Sezione
 from .mixins import StaffMixing
 from .forms import DiscussioneModelForm
 from django.http import HttpResponseRedirect
@@ -16,6 +16,7 @@ class CreaSezione(StaffMixing, CreateView):
 
 def visualizzaSezione(request, pk):
     sezione = get_object_or_404(Sezione, pk=pk)
+    # discussioni_sezione = Discussione.objects.filter(sezione_appartenenza=sezione).order_by("-data_creazione")
     context = {"sezione": sezione}
     return render(request, "forum/simula_sezione.html", context)
 
@@ -38,3 +39,10 @@ def creaDiscussione(request, pk):
         form = DiscussioneModelForm()
     context = {"form": form, "sezione": sezione}
     return render(request, "forum/crea_discussione.html", context)
+
+def visualizzaDiscussione(request, pk):
+    discussione = get_object_or_404(Discussione, pk=pk)
+    posts_discussione = Post.objects.filter(discussione=discussione)
+    context = {"discussione": discussione, "posts_discussione": posts_discussione}
+    return render(request, "forum/singola_discussione.html", context)
+
